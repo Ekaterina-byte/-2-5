71dde43f8dea6ea0b3fc90678942d61a4f8119c8
Ekaterina-byte <max-ka82@mail.ru>
2024-08-01 15:14:18 +0300

    домашняя работа по модулю 2

---
+++ module_3_1.py
@@ -0,0 +1,36 @@
+def string_info (string):
+    string_ = ()
+    string_ = (len(string), string.upper(), string.lower())
+    try:
+        string_info.calls_count += 1
+    except AttributeError:
+        string_info.calls_count = 1
+
+    return string_
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+
+def is_contans(string, list_to_search):
+    string = string.lower()
+    new_list = []
+    for i in list_to_search:
+        new_list.append(i.lower())
+    if string in new_list:
+        a = 'True'
+    else:
+        a = 'False'
+    try:
+        is_contans.calls_count += 1
+    except AttributeError:
+        is_contans.calls_count = 1
+    return(print(a))
+
+is_contans('Urban', ['ban', 'BaNaN', 'urBAN'])
+is_contans('cycle', ['recycling', 'cyclic' ])
+
+print(string_info.calls_count + is_contans.calls_count)
+
+#print(is_contans('Urban', ['ban', 'BaNaN', 'urBAN']))
+#print(is_contans('cycle', ['recycling', 'cyclic' ]))
+
