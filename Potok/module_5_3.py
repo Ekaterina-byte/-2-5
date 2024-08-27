008bc5adb950177521f88805bbc691fed7690119
Ekaterina-byte <max-ka82@mail.ru>
2024-08-27 15:18:56 +0300

    исправленное д/з модуль 5_3

--- Potok/module_5_3.py
+++ Potok/module_5_3.py
@@ -4,37 +4,45 @@ class House:
         self.number_of_floors = number_of_floors
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            return self.number_of_floors == other
+
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors < other.number_of_floors and self.number_of_floors == other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        return self.number_of_floors > other.number_of_floors and self.number_of_floors == other.number_of_floors
+        return not self.__lt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors < other.number_of_floors and self.number_of_floors > other.number_of_floors
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        self.number_of_floors += value
-        return self.number_of_floors
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        return self
 
     def __str__(self):
         return f'Название:  {self.name}, кол-во этажей: {self.number_of_floors}'
 
-    def __radd__(self, value):
-        self.number_of_floors += value
-        return self.__add__()
+    def __radd__(self, other):
+        return self.__add__(other)
 
-    def __iadd__(self,value):
-        self.number_of_floors += value
-        return self.__add__()
+    def __iadd__(self,other:int):
+        return self.__add__(other)
 
 
 h1 = House("ЖК Горский", 18)
@@ -51,7 +59,8 @@ print(h1<h2) #ge
 print(h1>h2) #ne
 h1.__add__(10) #add
 print(h1.__str__())
-h1.__add__(10) #radd
+#h1.__add__(10) #radd
+h1 += 10
 print(h1.__str__())
 h2.__add__(10) #iadd
 print(h2.__str__())
